26ce3177a993b737da034b5cfc5b5a8258af9c6d
harry1064 <harry.singh1064@gmail.com>
2019-05-10 00:04:45 +0800

    Initial Commit

---
+++ iDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iDelivery
+//
+//  Created by Harpreet Singh on 10/5/2019.
+//  Copyright © 2019 Harpreet Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
