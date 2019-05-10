9814754730517f64cfc5e2f6c971668af1959112
harry1064 <harry.singh1064@gmail.com>
2019-05-11 00:05:59 +0800

    Add Alamofire networking library using cocoapods.

---
+++ Pods/Target Support Files/Pods-iDeliveryTests/Pods-iDeliveryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iDeliveryTests : NSObject
+@end
+@implementation PodsDummy_Pods_iDeliveryTests
+@end
---
+++ Pods/Target Support Files/Pods-iDeliveryUITests/Pods-iDeliveryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iDeliveryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iDeliveryUITests
+@end
